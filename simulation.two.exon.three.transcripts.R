321457d6b973a71ee02be334fdea1444e147990b
Mu Wang <wang970@tsinghua.cs.purdue.edu>
2014-04-23 16:02:27 -0400

    R source code

---
+++ simulation.two.exon.three.transcripts.R
@@ -0,0 +1,133 @@
+# simulation
+
+
+
+generate.mpi=function(p1,k1,lam1,lam2){
+	tmp=rmultinom(k1,size=1,prob=c(p1,1-p1))
+	intensity1=rep(lam2,k1)
+      intensity1[as.numeric(tmp[1,])==1]=lam1
+	return(  rpois(k1,lambda=intensity1) )
+}
+
+
+# generate reads
+generate.filter.reads=function(start,end,l1,l2,p1){
+y.tmp=generate.mpi(p1,n,l1,l2)
+y<-numeric(n)
+for(i in start:end){
+	if(y.tmp[i]==0)
+		y[i]=0
+	else
+		y[i]=rbinom(1,size=y.tmp[i],prob=1)
+}
+return(y)
+}
+
+
+
+generate.data=function(intensity, proportion){
+# simulate data
+l=pi=matrix(0,nrow=3,ncol=2)
+l[,2]=intensity[1];  l[,1]=intensity[2:4]
+pi[,1]=proportion ;  pi[,2]=1-pi[,1]
+y=matrix(0,nrow=2,ncol=n)
+y1 <- generate.filter.reads(1,k1,     l[1,1],l[1,2],pi[1,1])
+y2 <- generate.filter.reads((k1+1), n,l[2,1],l[2,2],pi[2,1])
+y3 <- generate.filter.reads(1,n,      l[3,1],l[3,2],pi[3,1])
+y[1,1:k1]= y1[1:k1]+y3[1:k1]
+y[2,(k1+1):n]= y2[(k1+1):n]+y3[(k1+1):n]
+return(y=y)
+}
+
+
+
+
+res=list()
+
+for(ii in 1:100){
+res[[ii]]=list()
+l0=0.04
+l1=8
+l2=10
+l3=12
+p1=0.3
+p2=0.2
+p3=0.1
+k1=2000; n=4000
+
+
+#simulate 1  non-junction 
+set.seed(ii)
+
+y=generate.data(intensity=c(l0,l1,l2,l3),proportion=c(p1,p2,p3))
+write.matrix(y,file = "y.dat", sep = " ", blocksize)
+stop()
+apply(y,1,sum)
+
+par(mfrow=c(1,2),mar=c(2,2,2,2))
+for(i in 1:2){
+	plot(y[i,],pch=19,cex=0.9,ylim=range(y))
+	abline(v=k1,col="red");	abline(v=n,col="red")
+}
+
+
+N=dim(y)[1]; solve=1:3; NT=length(solve); solve; NT; N
+type=c("1-1","2-2");type	
+
+trans=list()
+trans[[1]]=c(1,3); trans[[2]]=c(2,3); trans
+start=c(1,k1+1);  end=c(k1,n) ;start;end
+
+
+	# multiple intial values
+	N.iter=10
+	intensity=list(0)
+	mixing=list(0)
+	loglikelihood=numeric(N.iter)
+	success=1
+	while(success <= N.iter){print(paste(ii,success,sep="-"))
+		tmp=any.exon(y)
+		intensity[[success]]=tmp$l
+		mixing[[success]]=tmp$p
+		loglikelihood[success]=tmp$likelihood
+		success=success+1
+	}
+
+	ind=which.max( loglikelihood );ind
+	print("estimated intensities are")
+    print(intensity[[ind]])
+	res[[ii]][[1]]= intensity[[ind]]
+	print("estimated mixing are")
+    print(mixing[[ind]])
+	res[[ii]][[2]]= mixing[[ind]]
+
+
+}
+
+
+#save(res,file="result_two_exon_three_transcripts.Rdata")
+#load("result_two_exon_three_transcripts.Rdata")
+
+
+lam1=numeric(0)
+lam2=numeric(0)
+lam3=numeric(0)
+
+for(i in 1:19){ 
+	lam1=c(lam1,res[[i]][[1]][1,1])
+	lam2=c(lam2,res[[i]][[1]][2,1])
+	lam3=c(lam3,res[[i]][[1]][3,1])
+}
+
+mean(lam1);mean(lam2);mean(lam3)
+sd(lam1);sd(lam2);sd(lam3)
+
+
+par(mfrow=c(1,3),mar=c(2.9,4,1,1))
+plot(lam1,cex=0.8,pch=19,col="red",ylab="lambda_12",ylim=c(4,19))
+plot(lam2,cex=0.8,pch=19,col="green",ylab="lambda_22",ylim=c(4,19))
+plot(lam3,cex=0.8,pch=19,col="blue",ylab="lambda_32",ylim=c(4,19))
+
+
+
+
---
+++ solve_two_exon.R
@@ -0,0 +1,231 @@
+
+
+
+
+
+# poission distribution
+p.fun=function(lambda,y){
+	return( exp(-lambda)*lambda^y )
+}
+
+
+# Q function ( intensity part )
+f.ind=function(beta,abc,start,end,exon,y){
+l=matrix(0,nrow=NT,ncol=2)
+l[,2]=beta[1]; l[,1]=beta[2:(1+NT)]
+if(length(abc)==2){
+	a=abc[1];b=abc[2]
+	tmp=0
+	for(i in 1:2){
+		for(j in 1:2){
+		tmp=tmp+sum( get(paste("Eu",i,j,exon,sep="")) * (-l[a,i]-l[b,j]+ y[start:end]*log(l[a,i]+l[b,j]) ) )
+		}
+	}
+}
+if(length(abc)==1){
+	a=abc[1]
+	tmp=0
+	for(i in 1:2){
+		tmp=tmp+sum( get(paste("Eu",i,exon,sep="")) * (-l[a,i]+ y[start:end]*log(l[a,i]) ) )
+	}
+}
+return(-tmp)
+}
+
+
+# Q function ( proportion part )
+pp.ind=function(beta,abc,start,end,exon){
+pi=matrix(0,nrow=NT,ncol=2)
+pi[,1]=beta[1:NT]; pi[,2]=1-pi[,1]
+if(length(abc)==2){
+a=abc[1];b=abc[2]
+tmp=0
+	for(i in 1:2){
+		for(j in 1:2){
+			tmp=tmp+sum( get(paste("Eu",i,j,exon,sep="")) * (log( pi[a,i]*pi[b,j] )     )     ) 	
+		}
+	}
+}
+if(length(abc)==1){
+	a=abc[1]
+	tmp=0
+	for(i in 1:2){
+		tmp=tmp+sum( get(paste("Eu",i,exon,sep="")) * (log( pi[a,i] )     )   ) 	
+	}
+}
+return(-tmp)
+}
+
+
+
+initialization=function(range){
+l=pi=matrix(0,nrow=NT,ncol=2)
+l[,1]=runif(NT,min(range),max(range))
+l[,2]=runif(1,0.2,1)
+pi[,1]=runif(NT,0.2,1)
+pi[,2]=1-pi[,1]
+return(list(l=l,pi=pi))
+}
+
+
+
+# update Eu.  global assignment
+update.Eu=function(l,pi,abc,start,end,exon,y){
+if(length(abc)==2){
+	a=abc[1];b=abc[2]
+	denom=0
+	for(i in 1:2){
+		for(j in 1:2){
+			denom=denom+p.fun(  ( l[a,i]+l[b,j] )  , y[start:end] )   *  pi[a,i]*pi[b,j]
+		}
+	}
+	for(i in 1:2){
+		for(j in 1:2){
+			tmp=p.fun(  l[a,i]+l[b,j]  , y[start:end] )   *  pi[a,i]*pi[b,j]
+			assign(paste("Eu",i,j,exon,sep=""),tmp/denom, envir = .GlobalEnv)
+		}
+	}
+}
+if(length(abc)==1){
+	a=abc[1]
+	denom=0
+	for(i in 1:2){
+		denom=denom+p.fun(  ( l[a,i] )  , y[start:end] )   *  pi[a,i]
+	}
+	for(i in 1:2){
+		tmp=p.fun(  l[a,i]  , y[start:end] )   *  pi[a,i]
+		assign(paste("Eu",i,exon,sep=""),tmp/denom, envir = .GlobalEnv)
+	}
+}
+}
+
+
+
+ff=function(beta){
+f=0
+for(i in 1:N){
+	if( length(trans[[i]]) >0){
+		f=f+f.ind(beta,abc=trans[[i]],start=start[i], end=end[i],exon=paste("e",i,sep=""),y[i,])
+	}
+}
+return(f)
+}
+
+
+pp=function(beta){
+tmp=0
+for(i in 1:N){
+	if( length(trans[[i]]) >0){
+		tmp=tmp+pp.ind(beta,abc=trans[[i]],start=start[i], end=end[i],exon=paste("e",i,sep=""))
+	}
+}
+return(tmp)
+}
+
+
+like.ind=function(l,pi,abc,start,end,exon,y){
+if(length(abc)==2){
+	a=abc[1];b=abc[2]
+	tmp=0
+	for(i in 1:2){
+		for(j in 1:2){
+			tmp=tmp+sum( get(paste("Eu",i,j,exon,sep="")) * (log(pi[a,i]*pi[b,j])-l[a,i]-l[b,j]+ y[start:end]*log(l[a,i]+l[b,j]) ) ) 					
+		}
+	}
+}
+if(length(abc)==1){
+	a=abc[1]
+	tmp=0
+	for(i in 1:2){
+		tmp=tmp+sum( get(paste("Eu",i,exon,sep="")) * (log(pi[a,i])-l[a,i]+ y[start:end]*log(l[a,i]) ) ) 					
+	}
+}
+return(tmp)
+}
+
+
+likelihood=function(l,pi){
+tmp=0
+for(i in 1:N){
+	if( length(trans[[i]]) >0){
+		tmp=tmp+like.ind(l,pi,abc=trans[[i]],start=start[i], end=end[i],exon=paste("e",i,sep=""),y[i,])
+	}
+}
+return(tmp)
+}
+
+
+
+
+
+any.exon=function(y){
+iter=1
+change.of.para=1
+eps=0.001
+pre=1e-5
+tmp=initialization(range=c(1,max(y)/2))
+l=tmp$l;l
+pi=tmp$pi;pi
+
+ l.new=matrix(0,nrow=NT,ncol=2);l.new
+pi.new=matrix(0,nrow=NT,ncol=2);pi.new[,1]=pre;pi.new[,2]=1-pi.new[,1];pi.new
+
+
+while(change.of.para>eps){ #print(iter);  
+#print(change.of.para)
+
+for(i in 1:N){ 
+	if( length(trans[[i]]) >0){
+		update.Eu(l,pi,abc=trans[[i]], start=start[i], end=end[i],exon=paste("e",i,sep=""),y[i,])
+	}
+}
+
+fit=optim( c(l[1,2],l[,1]) , ff, NULL, method = "L-BFGS-B" ,lower=pre   )
+l.new[,2]=fit$par[1]
+l.new[,1]=fit$par[2:(1+NT)]
+
+fitp=optim(as.vector(pi[,1]) , pp, NULL, method = "L-BFGS-B" ,lower=pre,upper=1-pre )
+pi.new[,1]=fitp$par;pi.new[,2]=1-pi.new[,1]
+
+change.of.para=sum(	abs(l-l.new)+ abs(pi-pi.new) 			 )
+
+l=l.new; pi=pi.new
+
+if(iter>500){
+	 l=matrix(0,nrow=NT,ncol=2);
+	pi=matrix(0,nrow=NT,ncol=2)
+	break
+}
+
+iter=iter+1
+}
+
+return(list(l=as.data.frame(l),pi=as.data.frame(pi),likelihood=likelihood(l,pi)))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+################################################################
+
